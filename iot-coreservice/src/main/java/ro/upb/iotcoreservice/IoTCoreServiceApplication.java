3d1e855c3bcdef1a0cc642355c1060f16c4b6d0a
alexdragnea <alexdg722@gmail.com>
2024-04-21 18:48:09 +0300

    Add Iot CoreService skeleton and workflows

---
+++ iot-coreservice/src/main/java/ro/upb/iotcoreservice/IoTCoreServiceApplication.java
@@ -0,0 +1,13 @@
+package ro.upb.iotcoreservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IoTCoreServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(IoTCoreServiceApplication.class, args);
+    }
+
+}
---
+++ iot-coreservice/src/test/java/ro/upb/iotcoreservice/IoTCoreServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package ro.upb.iotcoreservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IoTCoreServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
