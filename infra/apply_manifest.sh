9c7945bded5ab21c489b4dab67e8f16b1e3fe5bc
alexdragnea <alexdg722@gmail.com>
2024-04-26 21:10:29 +0300

    Add infrastructure files for discovery service

--- infra/apply_manifest.sh
+++ infra/apply_manifest.sh
@@ -2,7 +2,8 @@
 
 # Services
 services=(
-    "https://raw.githubusercontent.com/alexdragnea/UPB-Disertatie/master/infra/services/iot-coreservice.yaml"
+    "https://raw.githubusercontent.com/alexdragnea/UPB-Disertatie/master/infra/services/iot-core-service-svc.yaml"
+    "https://raw.githubusercontent.com/alexdragnea/UPB-Disertatie/master/infra/services/iot-discovery-service-svc.yaml"
 )
 
 for service in "${services[@]}"; do
@@ -12,19 +13,21 @@ done
 
 # Deployments
 deployments=(
-    "https://raw.githubusercontent.com/alexdragnea/UPB-Disertatie//master/infra/deployments/iot-coreservice.yaml"
+    "https://raw.githubusercontent.com/alexdragnea/UPB-Disertatie/master/infra/deployments/iot-core-service.yaml"
+    "https://raw.githubusercontent.com/alexdragnea/UPB-Disertatie/master/infra/deployments/iot-discovery-service.yaml"
 )
 
 sleep 5
 
 for deployment in "${deployments[@]}"; do
     kubectl apply -f "$deployment"
-    sleep 1
+    sleep 30
 done
 
 # Horizontal Pod Autoscaler (HPA)
 hpa_files=(
-    "https://raw.githubusercontent.com/alexdragnea/UPB-Disertatie//master/infra/hpa/iot-coreservice-hpa.yaml"
+    "https://raw.githubusercontent.com/alexdragnea/UPB-Disertatie//master/infra/hpa/iot-core-service-hpa.yaml"
+    "https://raw.githubusercontent.com/alexdragnea/UPB-Disertatie/master/infra/hpa/iot-discovery-service-hpa.yaml"
 )
 
 for hpa_file in "${hpa_files[@]}"; do
