9eff67a80c8912119d417eb4909d6770fefbb5c4
alexdragnea <alexdg722@gmail.com>
2024-04-28 19:24:12 +0300

    Add discovery service loadbalancer

--- iot-discovery-service/src/main/java/ro/upb/iotdiscoveryservice/IotDiscoveryServiceApplication.java
+++ iot-discovery-service/src/main/java/ro/upb/iotdiscoveryservice/IotDiscoveryServiceApplication.java
@@ -2,10 +2,12 @@ package ro.upb.iotdiscoveryservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @SpringBootApplication
 @EnableEurekaServer
+@EnableDiscoveryClient
 public class IotDiscoveryServiceApplication {
 
     public static void main(String[] args) {
