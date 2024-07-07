b0a3e220881e410ddc44e0594f93b62ce72310d9
alexdragnea <alexdg722@gmail.com>
2024-07-07 17:41:42 +0300

    Fix web client

--- iot-core-service/src/main/java/ro/upb/iotcoreservice/client/UserServiceClient.java
+++ iot-core-service/src/main/java/ro/upb/iotcoreservice/client/UserServiceClient.java
@@ -4,6 +4,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.cloud.client.loadbalancer.reactive.ReactorLoadBalancerExchangeFilterFunction;
 import org.springframework.stereotype.Component;
 import org.springframework.web.reactive.function.client.WebClient;
+import org.springframework.web.reactive.function.client.WebClientRequestException;
 import reactor.core.publisher.Mono;
 import ro.upb.common.dto.LoggedInDetails;
 
@@ -29,6 +30,15 @@ public class UserServiceClient {
                 .get()
                 .uri("/v1/iot-user/logged")
                 .retrieve()
-                .bodyToMono(LoggedInDetails.class);
+                .bodyToMono(LoggedInDetails.class)
+                .onErrorMap(
+                        WebClientRequestException.class,
+                        this::handleWebClientRequestException
+                );
+    }
+
+    private Throwable handleWebClientRequestException(WebClientRequestException ex) {
+        // Handle specific WebClientRequestException scenarios here
+        return new RuntimeException("Failed to retrieve user details from iot-user-service", ex);
     }
 }
