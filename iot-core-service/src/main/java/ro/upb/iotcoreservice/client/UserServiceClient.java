bb637585472b973c2d28c0ae7f5d3dd620eb5e7b
alexdragnea <alexdg722@gmail.com>
2024-07-07 18:27:31 +0300

    Use hardcoded uri for UserServiceClient

--- iot-core-service/src/main/java/ro/upb/iotcoreservice/client/UserServiceClient.java
+++ iot-core-service/src/main/java/ro/upb/iotcoreservice/client/UserServiceClient.java
@@ -1,10 +1,8 @@
 package ro.upb.iotcoreservice.client;
 
 import lombok.RequiredArgsConstructor;
-import org.springframework.cloud.client.loadbalancer.reactive.ReactorLoadBalancerExchangeFilterFunction;
 import org.springframework.stereotype.Component;
 import org.springframework.web.reactive.function.client.WebClient;
-import org.springframework.web.reactive.function.client.WebClientRequestException;
 import reactor.core.publisher.Mono;
 import ro.upb.common.dto.LoggedInDetails;
 
@@ -13,32 +11,13 @@ import ro.upb.common.dto.LoggedInDetails;
 public class UserServiceClient {
 
     private final WebClient.Builder webClientBuilder;
-    private final ReactorLoadBalancerExchangeFilterFunction lbFunction;
 
-    //    public Mono<LoggedInDetails> getUser(){
-//        return webClientBuilder.build()
-//                .get()
-//                .uri("http://iot-user-service/v1/iot-user/logged")
-//                .retrieve()
-//                .bodyToMono(LoggedInDetails.class);
-//    }
-//
-    public Mono<LoggedInDetails> getUser() {
-        return webClientBuilder.baseUrl("http://iot-user-service")
-                .filter(lbFunction)
-                .build()
+        public Mono<LoggedInDetails> getUser(){
+        return webClientBuilder.build()
                 .get()
-                .uri("/v1/iot-user/logged")
+                .uri("http://localhost:8003/v1/iot-user/logged")
                 .retrieve()
-                .bodyToMono(LoggedInDetails.class)
-                .onErrorMap(
-                        WebClientRequestException.class,
-                        this::handleWebClientRequestException
-                );
+                .bodyToMono(LoggedInDetails.class);
     }
 
-    private Throwable handleWebClientRequestException(WebClientRequestException ex) {
-        // Handle specific WebClientRequestException scenarios here
-        return new RuntimeException("Failed to retrieve user details from iot-user-service", ex);
-    }
 }
