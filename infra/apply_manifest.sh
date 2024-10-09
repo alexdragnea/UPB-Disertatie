f84e0bcdaf9ad8d2c724fa90e784ed076d4376eb
alexdragnea <alexdg722@gmail.com>
2024-10-09 22:29:56 +0300

    Fix k8s

--- infra/apply_manifest.sh
+++ infra/apply_manifest.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
 
-# helm repo add bitnami https://charts.bitnami.com/bitnami
-# helm repo update
-# helm install prometheus bitnami/kube-prometheus --namespace observability
+helm repo add bitnami https://charts.bitnami.com/bitnami
+helm repo update
+helm install prometheus bitnami/kube-prometheus --namespace observability
 
-# sleep 30
+sleep 30
 
-# helm install grafana bitnami/grafana --namespace observability
+helm install grafana bitnami/grafana --namespace observability
 
 # Kafka
 kafka=(
@@ -27,7 +27,7 @@ services=(
     "https://raw.githubusercontent.com/alexdragnea/UPB-Disertatie/master/infra/services/iot-discovery-service-svc.yaml"
     "https://raw.githubusercontent.com/alexdragnea/UPB-Disertatie/master/infra/services/iot-bridge-service-svc.yaml"
     "https://raw.githubusercontent.com/alexdragnea/UPB-Disertatie/master/infra/services/iot-gateway-service-svc.yaml"
-    # "https://raw.githubusercontent.com/alexdragnea/UPB-Disertatie/master/infra/services/service-monitor.yaml"
+    "https://raw.githubusercontent.com/alexdragnea/UPB-Disertatie/master/infra/services/service-monitor.yaml"
 )
 
 for service in "${services[@]}"; do
@@ -51,7 +51,7 @@ sleep 5
 
 for deployment in "${deployments[@]}"; do
     kubectl apply -f "$deployment"
-    sleep 30
+    sleep 60
 done
 
 # Horizontal Pod Autoscaler (HPA)
@@ -64,7 +64,7 @@ hpa_files=(
 
 for hpa_file in "${hpa_files[@]}"; do
     kubectl apply -f "$hpa_file"
-    sleep 1
+    sleep 5
 done
 
 # kubectl port-forward --namespace default svc/prometheus-kube-prometheus-prometheus 9090:9090
