a8ab1f7632a5b1ee06c21a6218db8e7b06433748
alexdragnea <alexdg722@gmail.com>
2025-03-03 22:33:59 +0200

    Update infra

--- infra/delete_manifest.sh
+++ infra/delete_manifest.sh
@@ -1,83 +1,86 @@
 #!/bin/bash
 
-# Define deployments
-deployments=(
-    "iot-dashboard-service"
-    "iot-discovery-service"
-    "iot-core-service"
-    "iot-bridge-service"
-    "iot-gateway-service"
-    "iot-user-service"
-    "redis"
-    "kafka"
-    "kafka-drop-deployment"
-    "zookeeper-deployment"
-    "influxdb"
-    "mongodb"
-)
+# # Define deployments
+# deployments=(
+#     "iot-dashboard-service"
+#     "iot-discovery-service"
+#     "iot-core-service"
+#     "iot-bridge-service"
+#     "iot-gateway-service"
+#     "iot-user-service"
+#     "redis"
+#     "kafka"
+#     "kafka-drop-deployment"
+#     "zookeeper-deployment"
+#     "influxdb"
+#     "mongodb"
+# )
 
-# Define hpas
-hpas=(
-    "hpa-iot-discovery-service"
-    "hpa-iot-core-service"
-    "hpa-iot-bridge-service"
-    "hpa-iot-gateway-service"
-    "hpa-iot-user-service"
-    "hpa-influxdb"
-)
+# # Define hpas
+# hpas=(
+#     "hpa-iot-discovery-service"
+#     "hpa-iot-core-service"
+#     "hpa-iot-bridge-service"
+#     "hpa-iot-gateway-service"
+#     "hpa-iot-user-service"
+#     "hpa-influxdb"
+# )
 
-# Define services
-services=(
-    "iot-dashboard-service"
-    "iot-discovery-service"
-    "iot-core-service"
-    "iot-bridge-service"
-    "iot-gateway-service"
-    "iot-user-service"
-    "redis"
-    "kafka-service"
-    "kafka-drop-service"
-    "zookeeper-service"
-    "influxdb"
-    "mongodb"
-)
+# # Define services
+# services=(
+#     "iot-dashboard-service"
+#     "iot-discovery-service"
+#     "iot-core-service"
+#     "iot-bridge-service"
+#     "iot-gateway-service"
+#     "iot-user-service"
+#     "redis"
+#     "kafka-service"
+#     "kafka-drop-service"
+#     "zookeeper-service"
+#     "influxdb"
+#     "mongodb"
+# )
 
-read -p "This will delete the deployments, services, and HPAs. Are you sure? (y/n): " confirmation
-if [ "$confirmation" != "y" ]; then
-    echo "Operation canceled."
-    exit 1
-fi
+# read -p "This will delete the deployments, services, and HPAs. Are you sure? (y/n): " confirmation
+# if [ "$confirmation" != "y" ]; then
+#     echo "Operation canceled."
+#     exit 1
+# fi
 
-# Delete deployments
-for deployment in "${deployments[@]}"; do
-    kubectl delete deployment "$deployment"
-    if [ $? -eq 0 ]; then
-        echo "Deployment '$deployment' deleted successfully."
-    else
-        echo "Error deleting deployment '$deployment'."
-    fi
-done
+# # Delete deployments
+# for deployment in "${deployments[@]}"; do
+#     kubectl delete deployment "$deployment"
+#     if [ $? -eq 0 ]; then
+#         echo "Deployment '$deployment' deleted successfully."
+#     else
+#         echo "Error deleting deployment '$deployment'."
+#     fi
+# done
 
-# Delete services
-for service in "${services[@]}"; do
-    kubectl delete service "$service"
-    if [ $? -eq 0 ]; then
-        echo "Service '$service' deleted successfully."
-    else
-        echo "Error deleting service '$service'."
-    fi
-done
+# # Delete services
+# for service in "${services[@]}"; do
+#     kubectl delete service "$service"
+#     if [ $? -eq 0 ]; then
+#         echo "Service '$service' deleted successfully."
+#     else
+#         echo "Error deleting service '$service'."
+#     fi
+# done
 
-# Delete hpas
-for hpa in "${hpas[@]}"; do
-    kubectl delete hpa "$hpa"
-    if [ $? -eq 0 ]; then
-        echo "HPA '$hpa' deleted successfully."
-    else
-        echo "Error deleting HPA '$hpa'."
-    fi
-done
+# # Delete hpas
+# for hpa in "${hpas[@]}"; do
+#     kubectl delete hpa "$hpa"
+#     if [ $? -eq 0 ]; then
+#         echo "HPA '$hpa' deleted successfully."
+#     else
+#         echo "Error deleting HPA '$hpa'."
+#     fi
+# done
 
+kubectl delete dployments --all
+kubectl delete services --all
+kubectl delete hpa --all 
 kubectl delete statefulset kafka
 kubectl delete statefulset zookeeper
 kubectl delete pvc --all
